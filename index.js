fa076b80eb675e3e6031a3807d6f4d8a0e60fb21
Kenzastra <100890561+Kenzastra@users.noreply.github.com>
2024-01-23 11:53:01 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import express from "express";
 import cors from "cors";
 import db from "./config/database.js";
+import fileUpload from "express-fileupload";
 import Users from "./models/userModel.js";
 import Barangs from "./models/barangModel.js";
+import Details from "./models/detailModel.js";
+import Penjualan from "./models/penjualanModel.js";
+import transaksiRoute from "./route/transaksiRoute.js";
+import barangRoute from "./route/barangRoute.js";
 
 const app = express();
 
-// db.sync(Barangs);
+// db.sync({alter:true});
 try {
     await db.authenticate();
     console.log("Database Connected")
@@ -19,5 +24,8 @@ app.use(cors(
     origin: 'http://localhost:3000'}
 ));
 app.use(express.json());
+app.use(fileUpload());
+app.use(transaksiRoute);
+app.use(barangRoute);
 
 app.listen(5000, () => console.log("Server running at port 5000"));
