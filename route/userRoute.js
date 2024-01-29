5b553a99df1d9479a3aa0c040a4741a920262dad
Kenzastra <100890561+Kenzastra@users.noreply.github.com>
2024-01-29 21:19:26 +0700

    Add files via upload

--- route/userRoute.js
+++ route/userRoute.js
@@ -6,13 +6,14 @@ import {
     deleteUser,
     getUserById
 } from "../controllers/userController.js";
+import { verifyUser , adminOnly} from "../middleware/authUser.js";
 
 const route = express.Router()
 
-route.get("/user", getUser);
-route.get("/user/:id_user",getUserById)
-route.post("/user", addUser);
-route.patch("/user/:id_user", updateUser);
-route.delete("/user/:id_user",deleteUser);
+route.get("/user", verifyUser,adminOnly,getUser);
+route.get("/user/:id_user",verifyUser,adminOnly,getUserById)
+route.post("/user",verifyUser,adminOnly,addUser);
+route.patch("/user/:id_user", verifyUser,adminOnly,updateUser);
+route.delete("/user/:id_user",verifyUser,adminOnly,deleteUser);
 
 export default route;
